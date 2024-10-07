b85111844a5c9e62578db8b50dd95868d68a6189
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 08:35:19 -0600

    Creacion de un bucle que se encarga de pedir las asistencias totales

--- sistema_de_asis/Program.cs
+++ sistema_de_asis/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Trabajo Realizado por Joseph Saballos 1M2-S
+
+using System;
 namespace sistema_de_asis
 {
     class program
@@ -15,6 +17,25 @@ namespace sistema_de_asis
             Console.WriteLine("Bienvenido al Sistema de Asistencias de la Universidad Nacional de Ingenieria ");
             Console.WriteLine();
 
+
+            Console.WriteLine("Ingrese la Cantidad de Estudiantes");
+            int estudiante = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+
+            // Bucle para pedir  las asistencias totales
+            do
+            {
+                Console.WriteLine("Ingrese la Asistencia Total: ");
+                asistenciasTotales = Convert.ToInt32(Console.ReadLine());
+                if (asistenciasTotales < 0)
+                {
+                    Console.WriteLine("Dato erroneo, Ingrese un numero Positivo Por favor");
+                }
+
+            } while (asistenciasTotales < 0);
 
 
 
